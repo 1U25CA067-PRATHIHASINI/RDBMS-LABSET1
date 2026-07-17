7423e4c5efaaa7b0442bbee5da6164c60b4948cb
1U25CA067-PRATHIHASINI <hasinivijayakumar18@gmail.com>
2026-07-17 12:33:01 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE CollegeDB;
+USE CollegeDB;
+CREATE TABLE DEPARTMENT(DepartmentID int primary key, DepartmentName Varchar(10), HOD varchar(10));
+
 -- Write your SQL here
 
 -- Create Database
