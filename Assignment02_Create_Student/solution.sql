6d249364da60d756e708134fb77c93e9fbfb8066
1U25CA067-PRATHIHASINI <hasinivijayakumar18@gmail.com>
2026-07-17 12:59:10 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
+CREATE Table Student(StudentID INT Primary key,StudentName VARCHAR(20) NOT NULL,DOB DATE,Gender VARCHAR(10),DepartmentID INT (5));
+DESC Student
 
 -- Create Student table
 
