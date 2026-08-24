27b69b490118c3159f26b981a98139aebbbd97b2
1U25CA067-PRATHIHASINI <hasinivijayakumar18@gmail.com>
2026-08-24 10:40:15 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -3,7 +3,13 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
 -- Create Course table
+CREATE TABLE Course(courseID VARCHAR(20),courseName VARCHAR(20),credits INT(20) PRIMARY KEY,DepartmentID VARCHAR(20));
 
 -- Insert three records
+INSERT INTO Course(courseID,courseName,credits,DepartmentID) VALUES (10,"BCA",12,1);
+INSERT INTO Course(courseID,courseName,credits,DepartmentID) VALUES (20,"BCA",13,1);
+INSERT INTO Course(courseID,courseName,credits,DepartmentID) VALUES (30,"BCA",14,1);
 
 -- Display structure
+SELECT*FROM Course;
+DESC Course;
